b6c40d291d0dd50def16a71e8df609eb9e667d51
Alexander Zinin <spore_09@mail.ru>
2019-06-23 16:56:40 +0900

    change: remove getters from Key

--- keys/Key.js
+++ keys/Key.js
@@ -2,17 +2,9 @@ class Key {
     constructor(name, meta) {
         this.name = name;
         this.meta = meta;
-    }
 
-    get readableName() {
-        return this.isLegacy ? this.name : this.meta.name;
-    }
-
-    get isLegacy() {
-        return !this.meta;
-    }
-
-    static async loadFromPath(fullpath, name = 'N/D') {
+        this.isLegacy = !this.meta;
+        this.readableName = this.isLegacy ? this.name : this.meta.name;
     }
 }
 
