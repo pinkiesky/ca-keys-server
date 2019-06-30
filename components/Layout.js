560137eca8d858cf7a42bf8a1e5e177532647406
Alexander Zinin <spore_09@mail.ru>
2019-06-30 22:16:59 +0900

    add: styles for html, body

--- components/Layout.js
+++ components/Layout.js
@@ -2,12 +2,21 @@ const layoutStyle = {
     margin: 20,
     padding: 20,
     border: '1px solid #DDD'
-  };
+};
   
-  const Layout = (props) => (
-    <div style={layoutStyle}>
-      {props.children}
-    </div>
-  );
-  
-  export default Layout;
+const Layout = (props) => (
+  <div style={layoutStyle}>
+    {props.children}
+    <style jsx global>{`
+      html, body {
+        font-family: -apple-system,BlinkMacSystemFont,'Segoe UI','Roboto','Oxygen','Ubuntu', 'Cantarell','Fira Sans','Droid Sans','Helvetica Neue',sans-serif;
+        color: #333;
+        background-color: #fefefe;
+        margin: 0;
+        padding: 0;
+      }
+    `}</style>
+  </div>
+);
+
+export default Layout;
