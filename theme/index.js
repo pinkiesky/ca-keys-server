970579f2eee40d00cf0210abfa17eade3ae3e9b9
Alexander Zinin <spore_09@mail.ru>
2019-07-01 02:40:02 +0900

    add: lighter color in theme

--- theme/index.js
+++ theme/index.js
@@ -3,6 +3,7 @@ const colors = {
   surface: 'white',
   primary: '#e65100',
   primaryLight: '#ff833a',
+  primaryLigher: '#FFE2D1',
   primaryDark: '#ac1900',
   secondary: '#556eb3',
   border: '#eee',
