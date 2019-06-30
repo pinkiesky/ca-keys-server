d3fd799bfef39e542c7d4f1159c83a57774569f3
Alexander Zinin <spore_09@mail.ru>
2019-07-01 02:40:51 +0900

    fix: wrong size for layout

--- components/Layout.js
+++ components/Layout.js
@@ -10,7 +10,7 @@ const layoutStyle = {
 
 const Layout = ({ children }) => (
   <div style={layoutStyle}>
-    {children}
+    <div style={{ width: '100%', maxWidth: '600px' }}>{children}</div>
     <style jsx global>
       {`html, body {
         font-family: -apple-system,BlinkMacSystemFont,'Segoe UI','Roboto','Oxygen','Ubuntu', 'Cantarell','Fira Sans','Droid Sans','Helvetica Neue',sans-serif;
