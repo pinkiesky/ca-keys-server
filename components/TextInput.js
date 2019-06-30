3661a43ec74a2641a71585586dc9670487796eab
Alexander Zinin <spore_09@mail.ru>
2019-06-30 22:15:56 +0900

    add: TextInput

---
+++ components/TextInput.js
@@ -0,0 +1,24 @@
+import { Fragment } from 'react';
+import css from 'styled-jsx/css';
+
+
+const style = css`
+input[type=text] {
+    border-radius: 3px 3px 0px 0px;
+    padding: .5rem;
+    background-color: #eee;
+    border: none;
+    border-bottom: 2px solid gray;
+}
+
+input[type=text]:focus {
+    border-bottom: 2px solid blue;
+}`
+
+
+export default function TextInput(props) {
+    return <Fragment>
+        <input {...props} type='text'/>
+        <style jsx>{style}</style>
+    </Fragment>;
+}
--- components/keysTable/FilterableKeysTable.js
+++ components/keysTable/FilterableKeysTable.js
@@ -1,12 +1,13 @@
 import KeysTable from './KeysTable';
 import { useState } from 'react'
+import TextInput from '../TextInput';
 
 
 export default function FilterableKeysTable({ keys, actions }) {
     const [filter, setFilter] = useState('');
 
     return <div>
-        <div><input name='name' placeholder='Search by name' onChange={ev => setFilter(ev.target.value)}/></div>
+        <TextInput name='name' placeholder='Search by name' onChange={ev => setFilter(ev.target.value)}/>
         <KeysTable keys={keys} filter={filter} actions={actions}/>
     </div>
 }
